78e6a41f8bbf55759296769f9844a92579f74f7f
suchithgali <sgali@ucmerced.edu>
2025-10-23 21:18:30 -0700

    created if branch to play the music, turn on focus mode, and disbale it if certain keys pressed

--- main.cpp
+++ main.cpp
@@ -22,5 +22,31 @@ int main(){
     return -1;
   }
 
+  std::string isWorking = "N";
+  std::cout << "Are you working? Y/N: ";
+  std::cin >> isWorking;
+
+  //doSleep()
+  //sleep_until(system_clock::now() + minutes(5));
+  
+
+  if (isWorking == "Y"){
+    std::string warningText = "Warning. Warning. Procrastination Mode enabling in One minute.";
+    std::string command = "say -v Fred -r 200 \"" + warningText + "\"";
+    system(command.c_str());
+    std::cout << "Playing siren. Press 's' and Enter to stop.\n";
+    music.play();
+
+    char stopKey;
+    std::cin >> stopKey;
+    if (stopKey == 's') {
+      music.stop();
+      system("osascript -e 'tell application \"Shortcuts\" to run shortcut \"Disable ZenMode\"'");
+      return 0;
+    }
+    std::this_thread::sleep_for(music.getDuration());
+    system("osascript -e 'tell application \"Shortcuts\" to run shortcut \"ZenMode\"'");
+  }
+
 
 }
